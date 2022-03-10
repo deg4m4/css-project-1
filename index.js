45c6804113dbce0de617beefa3bbeb5eeada7d7f
theparthka <parthka.2005@gmail.com>
2022-03-10 21:38:34 +0530

    make main page

---
+++ index.js
@@ -0,0 +1,13 @@
+/* var b = document.body
+var mouse = document.getElementsByClassName("mouse")[0]
+
+b.onmousemove = (e) => {
+    console.log(e);
+    mouse.style.opacity = 1;
+    mouse.style.top = e.layerY + "px";
+    mouse.style.left = e.layerX + "px";
+}
+
+b.onmouseout = (e) => {
+    mouse.style.opacity = 0;
+} */
