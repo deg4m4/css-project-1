d9406aa44ed95253bc4fa565a197bdde10d59833
theparthka <parthka.2005@gmail.com>
2022-03-11 21:28:16 +0530

    main page level 0

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var gmx = document.getElementsByClassName("gmx")
 for (const b of gmx) {
 
     b.style.height = b.getBoundingClientRect().width + "px";
-    
+
     b.addEventListener("mousemove", (e) => {
 
         var x = e.clientX - b.getBoundingClientRect().x;
@@ -20,4 +20,17 @@ for (const b of gmx) {
         b.children[0].style.opacity = 0;
     })
 
+}
+
+document.body.onscroll = (e) => {
+    console.log(123);
+    if (document.documentElement.scrollTop > 200) {
+        document.body.classList.add("a-body")
+    } else {
+        document.body.classList.remove("a-body")
+    }
+}
+
+document.getElementsByClassName("bx-menu")[0].onclick = () => {
+    document.getElementsByClassName("menu-con")[0].classList.toggle("show")
 }
